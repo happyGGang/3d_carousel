a5b861be1ed6b517a67a26e6b69fb809cd1967e6
happyGGang <joy.gang@dxworks.kr>
2022-10-25 23:05:41 +0900

    [update] 캐러셀 동작 구현 완료

--- js/script.js
+++ js/script.js
@@ -4,4 +4,24 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+  const cellCount = 6
+  let selectedIndex = 0
+  const prevButton = get('.prev_button')
+  const nextButton = get('.next_button')
+  const carousel = get('.carousel')
+
+  const rotateCarousel = () => {
+    const angel = (selectedIndex / cellCount) * -360
+    carousel.style.transform = 'translateZ(-346px) rotateY(' + angels + 'deg)'
+  }  
+
+  prevButton.addEventListener('click', () => {
+    selectedIndex--
+    rotateCarousel()
+  })
+
+  nextButton.addEventListener('click', () => {
+    selectedIndex++
+    rotateCarousel()
+  })
 })()
